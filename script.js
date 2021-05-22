6cfef2c9ac7fc352038a9c9ae2d150f38d6d58a2
Lorena C <lorena@eunknown.org>
2021-05-21 20:46:49 -0700

    refactor fetch

--- script.js
+++ script.js
@@ -4,28 +4,40 @@ const errContainer = document.querySelector('.main');
 
 quoteBtn.addEventListener('click', generateQuote);
 
-generateQuote();
+// generateQuote();
 
 const renderError = function (msg) {
   errContainer.insertAdjacentText('beforebegin', msg);
 };
 
-function generateQuote() {
-  const config = {
-    headers: {
-      Accept: 'application/json',
-    },
-  };
+const api_url = 'https://seinfeld-quotes.herokuapp.com/random';
 
-  fetch('https://seinfeld-quotes.herokuapp.com/random', config)
-    .then((res) => res.json())
-    .then((data) => {
-      quoteEl.innerHTML = data.quote;
-    })
-    .catch((err) => {
-      console.error(`${err} 💥💥`);
-      renderError(`Something went wrong 💥💥 ${err.message}. Try again!`);
-    });
+async function generateQuote() {
+  const response = await fetch(api_url);
+  const data = await response.json();
+  const { quote, author } = data;
+  quoteEl.innerHTML = quote;
 }
+
+generateQuote();
+
+// function generateQuote() {
+//   const config = {
+//     headers: {
+//       Accept: 'application/json',
+//     },
+//   };
+
+//   fetch('https://seinfeld-quotes.herokuapp.com/random', config)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       // quoteEl.innerHTML = data.quote;
+
+//     })
+//     .catch((err) => {
+//       console.error(`${err} 💥💥`);
+//       renderError(`Something went wrong 💥💥 ${err.message}. Try again!`);
+//     });
+// }
 
 // data.author
