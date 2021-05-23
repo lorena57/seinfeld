f39e658edbc983256052afb7e6847657b9eb32a0
Lorena C <lorena@eunknown.org>
2021-05-22 18:40:06 -0700

    create function to verify character quote

--- script.js
+++ script.js
@@ -2,6 +2,24 @@ const quoteEl = document.getElementById('quote');
 const quoteBtn = document.getElementById('quotebutton');
 const errContainer = document.querySelector('.main');
 
+function checkButtonEqualQuote(author) {
+  console.log(author);
+  let buttons = document.querySelectorAll('.btn');
+  buttons.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+      //Trying to tie in button character to quote
+      let btnName = e.target.value;
+      if (btnName === author) {
+        alert('ok');
+      } else {
+        alert('wrong answer');
+      }
+
+      // alert(e.target.value);
+    });
+  });
+}
+
 quoteBtn.addEventListener('click', generateQuote);
 
 const api_url = 'https://seinfeld-quotes.herokuapp.com/random';
@@ -13,6 +31,7 @@ async function generateQuote() {
   console.log(quote);
   console.log(author);
   quoteEl.innerHTML = quote;
+  checkButtonEqualQuote(author);
 }
 
 const renderError = function (msg) {
@@ -20,10 +39,15 @@ const renderError = function (msg) {
 };
 
 generateQuote().catch((err) => {
-  renderError(`Something went wrong ${err.message}. Try again!`);
+  console.error(`${err} 💥💥`);
+  renderError(`Something went wrong 💥💥 ${err.message}. Try again!`);
 });
 
-//Old code
+// .catch((err) => {
+//   renderError(`Something went wrong ${err.message}. Try again!`);
+// });
+
+// Old code
 // function generateQuote() {
 //   const config = {
 //     headers: {
@@ -34,8 +58,8 @@ generateQuote().catch((err) => {
 //   fetch('https://seinfeld-quotes.herokuapp.com/random', config)
 //     .then((res) => res.json())
 //     .then((data) => {
+//       console.log(data);
 //       // quoteEl.innerHTML = data.quote;
-
 //     })
 //     .catch((err) => {
 //       console.error(`${err} 💥💥`);
